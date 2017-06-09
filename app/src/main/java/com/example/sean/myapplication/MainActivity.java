10d55127a92e846ed80d8b46d5a0b9445af4abca
Sean Blair <seandibango@gmail.com>
2017-06-08 19:15:10 -0700

    Adjust output volume, and change tempo proof of concept

--- app/src/main/java/com/example/sean/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sean/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.sean.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +16,9 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.sean.myapplication.MESSAGE";
+
+    private boolean beepOn = false;
+    private boolean fast = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void startBeep(View view) {
+        if (beepOn) {
+            return;
+        }
+
+        beepOn = true;
         Thread mThread = new Thread (new Runnable() {
             public void run() {
                 android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
@@ -41,10 +51,15 @@ public class MainActivity extends AppCompatActivity {
 
                 MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.beep5);
 
-                while (true) {
+                while (beepOn) {
+
                     mp.start();
                     try {
-                        Thread.sleep(1000);
+                        int timeToSleep = 1000;
+                        if (fast) {
+                            timeToSleep = 100;
+                        }
+                        Thread.sleep(timeToSleep);
                     } catch (InterruptedException e) {
                     }
                 }
@@ -52,5 +67,33 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mThread.start();
+    }
+
+    public void stopBeep(View view) {
+        beepOn = false;
+    }
+
+    public void slow(View view) {
+        fast = false;
+    }
+
+    public void fast(View view) {
+        fast = true;
+    }
+
+    public void setVolumeHigh(View view) {
+        AudioManager audioManager =
+                (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                50, 0);
+    }
+
+    public void setVolumeLow(View view) {
+        AudioManager audioManager =
+                (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                5, 0);
     }
 }
