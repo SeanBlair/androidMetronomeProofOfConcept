ab4b47fb85c241124608d52c1dbe63cf5f769ad5
Sean Blair <seandibango@gmail.com>
2017-06-08 18:25:56 -0700

    Basic periodic beep implemented

--- app/src/main/java/com/example/sean/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sean/myapplication/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.sean.myapplication;
 
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.sean.myapplication.MESSAGE";
@@ -18,11 +23,34 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
+
         // Do something in response to button
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+
+    public void startBeep(View view) {
+        Thread mThread = new Thread (new Runnable() {
+            public void run() {
+                android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
+
+
+                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.beep5);
+
+                while (true) {
+                    mp.start();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        });
+
+        mThread.start();
     }
 }
